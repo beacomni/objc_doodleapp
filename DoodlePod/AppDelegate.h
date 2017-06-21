f249fb02d5679b04b20d7461308e930d10f2d05d
beacomni <beacomni@DSI-Yellows-MacBook-Pro.local>
2017-06-21 11:55:27 -0500

    Initial Commit

---
+++ DoodlePod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoodlePod
+//
+//  Created by beacomni on 6/21/17.
+//  Copyright © 2017 beacomni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
